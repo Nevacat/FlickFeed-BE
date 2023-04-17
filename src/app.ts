c0993f9ab6912f5ddb357a6ae8483188c7ef2700
Nevacat <nevacattery217@naver.com>
2023-04-17 21:05:28 +0900

    feat: 서버 구축, DataBase연결, 이미지 업로드 연결

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import express from 'express';
+import { myDataBase } from './db';
+import cors from 'cors';
+import { upload } from './uploadS3';
+
+export const tokenList = {};
+
+myDataBase
+  .initialize()
+  .then(() => {
+    console.log('DataBase has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during DataBase initialization:', err);
+  });
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(
+  cors({
+    origin: true, // 모두 허용
+  })
+);
+
+app.post(
+  '/upload',
+  upload.fields([
+    { name: 'userImg', maxCount: 1 },
+    { name: 'postImg', maxCount: 1 },
+  ]),
+  (req, res) => {
+    res.json(req.file).send('파일이 정상적으로 업로드 되었습니다.');
+  }
+);
+
+app.listen(3000, () => {
+  console.log('Express server has started on port 3000');
+});
