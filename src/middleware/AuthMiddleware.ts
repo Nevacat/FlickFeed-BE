48a283b8df9fad688aba522e83f18222f197740b
Nevacat <nevacattery217@naver.com>
2023-04-18 15:02:35 +0900

    feat: 토큰 미들웨어 생성
    유저의 토큰을 받아서 미들웨어에서 유저정보를 req에 같이 보내줌.

---
+++ src/middleware/AuthMiddleware.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express"
+import { verify } from "jsonwebtoken"
+
+export interface TokenPayload {
+  email: string,
+  username: string,
+  id:number,
+}
+
+export interface JwtRequest extends Request {
+  decoded?: TokenPayload
+}
+
+export class AuthMiddleware {
+  static verifyToken = (req:JwtRequest, res:Response, next: NextFunction)=>{
+    const authHeader = req.headers['authorization']
+    const token = authHeader&&authHeader.split(' ')[1]
+    if(!token){
+      return res.status(403).send('토큰이 없습니다.')
+    }
+    try{
+      const decoded = verify(token, process.env.SECRET_ATOKEN) as TokenPayload
+      req.decoded = decoded
+    } catch(error){
+      return res.status(401).send('유효하지 않은 토큰입니다.')
+    }
+    return next()
+  }
+}
