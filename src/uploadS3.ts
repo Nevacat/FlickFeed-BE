f8247327433e5577948726e01a4f934019cd1f9d
Nevacat <nevacattery217@naver.com>
2023-04-18 21:23:11 +0900

    Docs: upload 버킷 재설정
    버킷명 입력 및 코드 수정

--- src/uploadS3.ts
+++ src/uploadS3.ts
@@ -13,14 +13,15 @@ const s3 = new S3Client({
   region: "ap-northeast-2",
   credentials:{
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_KEY
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
   }
 })
 
 export const upload = multer({
   storage: multerS3({
     s3,
-    bucket: 'public-read',
+    bucket: 'blackcat-bucket',
+    acl: 'public-read',
     contentType: multerS3.AUTO_CONTENT_TYPE,
     key: function(req:Request, file:Express.Multer.File, cb: KeyCallback){
       cb(null, Date.now() + "-" + file.originalname)
