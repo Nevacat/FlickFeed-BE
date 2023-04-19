5b5cb8478770ab37246aab18ad1ad8142c4e8a90
Nevacat <nevacattery217@naver.com>
2023-04-19 19:11:21 +0900

    feat: 댓글 삭제 API 로직 구현 및 라우터 연결
    댓글을 삭제할 수 있습니다.

--- src/controller/CommentController.ts
+++ src/controller/CommentController.ts
@@ -30,4 +30,23 @@ export class CommentController {
       console.error(err);
     }
   };
+  static deleteComment = async (req:JwtRequest, res:Response) => {
+    const {id: userId} = req.decoded
+    const {id:commentId} = req.body
+    const currentComment = await myDataBase.getRepository(Comment).findOne({
+      where:{id:commentId},
+      relations:{
+        author: true
+      }
+    })
+    if(!currentComment || userId !== currentComment.author.id){
+      return res.status(401).send("댓글이 존재하지 않거나 옳바른 사용자가 아닙니다.")
+    }
+    try{
+      const result = await myDataBase.getRepository(Comment).delete(commentId)
+      return res.send({result,message:"성공적으로 삭제되었습니다."})
+    }catch(err){
+      console.error(err)
+    }
+  }
 }
