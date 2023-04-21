6ce778bbdb693efe9fa60b277ade59dfe74c2ddf
Nevacat <nevacattery217@naver.com>
2023-04-21 20:14:33 +0900

    feat: 단일 포스터 조회

--- src/controller/PostController.ts
+++ src/controller/PostController.ts
@@ -3,18 +3,20 @@ import { Post } from "../entity/Post";
 import { myDataBase } from "../db";
 import { JwtRequest } from "../middleware/AuthMiddleware";
 import { User } from "../entity/User";
+import { Like } from "../entity/Like";
 
 interface MulterS3Request extends JwtRequest{
   file: Express.MulterS3.File
 }
 
 export class PostController{
+  //게시물 작성
   static createPost = async(req:MulterS3Request, res:Response)=>{
     const {place = '', content,} = req.body
     const {location: postImg} = req.file
     console.log(postImg)
     const {id: userId} = req.decoded
-    if(!postImg || req.body){
+    if(!postImg || !req.body){
       return res.status(400).json({error:"이미지 또는 게시글을 확인해주세요."})
     }
     const user = await myDataBase.getRepository(User).findOne({
@@ -29,11 +31,12 @@ export class PostController{
     newPost.author = user
     try{
       const createdPost = await myDataBase.getRepository(Post).insert(newPost)
-      return res.status(201).send({message:"성공적으로 글이 작성되었습니다."})
+      return res.status(201).json({message:"성공적으로 글이 작성되었습니다."})
     }catch(err){
       return console.error(err)
     }
   }
+  // 모든 게시물 가져오기
   static getPosts = async (req:Request, res:Response) => {
     const posts = await myDataBase.getRepository(Post).find({
       select:{
@@ -52,14 +55,29 @@ export class PostController{
           }
         }
       },
-      relations:["author","comments","comments.author"]
+      relations:["author","comments","comments.author","likes","likes.user"]
     })
     if(!posts || posts.length===0){
-      return res.status(404).send({error:"게시물이 존재하지 않습니다."})
+      return res.status(404).json({error:"게시물이 존재하지 않습니다."})
     }
-    res.send(posts)
+    res.json(posts)
   }
-  static getPost = async (req:JwtRequest, res:Response) => {
+  // 단일 게시물
+  static getPost = async (req:Request, res:Response) => {
+    const {id: postId} = req.params
+    const comments = await myDataBase.getRepository(Post).findOne({
+      where:{
+        id: postId
+      },
+      relations:["comments","comments.author","likes","likes.user"]
+    })
+    if(!comments){
+      return res.status(404).json({error:"존재하지 않습니다."})
+    }
+    res.status(200).json(comments)
+  }
+  // 사용자 게시물
+  static getUserPost = async (req:JwtRequest, res:Response) => {
     const {id: userId} = req.decoded
     const posts = await myDataBase.getRepository(Post).find({
       where:{
@@ -76,12 +94,38 @@ export class PostController{
       },
       relations:{
         author: true,
-        comments: true
       }
     })
     if(!posts || posts.length===0){
-      return res.status(404).send({error:"게시물이 존재하지 않습니다."})
+      return res.status(404).json({error:"게시물이 존재하지 않습니다."})
     }
-    res.send(posts)
+    res.json(posts)
+  }
+  // 좋아요
+  static likePost =async (req:JwtRequest, res:Response) => {
+    const {id:userId} = req.decoded
+    const user = await myDataBase.getRepository(User).findOne({
+      where:{
+        id: userId
+      }
+    })
+    const isExist = await myDataBase.getRepository(Like).findOne({
+      where: {
+        post: { id: req.params.id },
+        user: { id: user.id }, // {id: 유저아이디} 가 요청 시에 필요
+      },
+    })
+    if(!isExist){
+      const post = await myDataBase.getRepository(Post).findOneBy({
+        id: req.params.id
+      })
+      const like = new Like()
+      like.post = post
+      like.user = user
+      await myDataBase.getRepository(Like).insert(like)
+    }else{
+      await myDataBase.getRepository(Like).remove(isExist)
+    }
+    return res.send({message:'success'})
   }
 }
--- src/router/post.ts
+++ src/router/post.ts
@@ -5,8 +5,9 @@ import { upload } from "../uploadS3";
 
 const routes = Router()
 
-routes.post('/posts',AuthMiddleware.verifyToken,upload.single("postImg") ,PostController.createPost)
-routes.get('/posts',PostController.getPosts)
-routes.get('/posts/me',PostController.getPost)
-
+routes.post('/posts', AuthMiddleware.verifyToken,upload.single("postImg") ,PostController.createPost)
+routes.get('/posts', PostController.getPosts)
+routes.get('/posts/me', PostController.getUserPost)
+routes.get('/posts/:id', PostController.getPost)
+routes.post('/posts/:id', AuthMiddleware.verifyToken, PostController.likePost)
 export default routes
